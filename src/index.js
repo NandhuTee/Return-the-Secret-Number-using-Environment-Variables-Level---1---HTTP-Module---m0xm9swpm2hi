ca8732bf5347871fb77689506ec803924af1348b
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2025-03-18 20:58:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 
-//dotenv.config();
-
-//app.listen(3000, () => console.log('Server running......'));
-
-
-
+dotenv.config(); // Ensure environment variables are loaded
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
-
